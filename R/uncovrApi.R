9ed6cd8e095eb409973bfd47632168b215df7105
Sidharth Macherla <msidharthrasik@gmail.com>
2022-05-01 17:41:05 +1200

    uncovr functions updated
    Final source code for the version 1.5.0 that includes the uncovr API functions.

--- R/uncovrApi.R
+++ R/uncovrApi.R
@@ -3,7 +3,7 @@
 #' @param body A json with the details of the independent and dependent variable.
 #' @param key An alpha numeric. This is the subscription key that can be sourced from the developer portal of uncovr API available at \emph{https://foyi.developer.azure-api.net/}.
 #' @importFrom httr POST add_headers
-#' @return A .
+#' @return A json.
 #' @details The purpose of this function can be best understood when explained within the context that is given below. There is a closed source SaaS(Software as a Service) software named \emph{uncovr} that provides an API(Application Programming Interface). In its current state, the SaaS software is free to use with some constraints around the volume of data and the frequency of API calls. One of the functions of \emph{uncovr} API takes an input of number of observations i.e. rows and number of independent variables namely columns and gives an output. This function \emph{uncovrApi} makes the connection to \emph{uncovr} API and sources the response.
 
 #' #function to call uncovr api
